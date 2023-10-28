003c6021b52ddd5bc80c0533c6db2d0158e0725d
Yaroslav Tsybulia <60847005+YaroslavTsybulia@users.noreply.github.com>
2023-10-28 15:10:32 -0400

    Summary

--- Title Screen.js
+++
@@ -1,9 +0,0 @@
-// Add this JavaScript to make the image appear smoothly
-window.addEventListener('load', function() 
-{
-	setTimeout( function() 
-	{
-        const smoothImage = document.getElementById('smooth-image');
-        smoothImage.style.opacity = '1'; // Change opacity to 1 to make it visible smoothly
-    }, 500); // Adjust the delay in milliseconds (1000ms = 1 seconds)
-});
